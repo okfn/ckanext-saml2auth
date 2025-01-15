de561a59c531774103f45749eb713de03a71d3d8
avdata99 <andres@data99.com.ar>
2025-01-15 11:43:59 -0300

    Update script

--- bin/setup-ckan.bash
+++ bin/setup-ckan.bash
@@ -10,31 +10,15 @@ sudo apt-get install xmlsec1 libxmlsec1-dev
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-if [ $CKANVERSION == 'master' ]
-then
-    echo "CKAN version: master"
-else
-    CKAN_TAG=$(git tag | grep ^ckan-$CKANVERSION | sort --version-sort | tail -n 1)
-    git checkout $CKAN_TAG
-    echo "CKAN version: ${CKAN_TAG#ckan-}"
-fi
 
-# install the recommended version of setuptools
-if [ -f requirement-setuptools.txt ]
-then
-    echo "Updating setuptools..."
-    pip install -r requirement-setuptools.txt
-fi
+CKAN_TAG=$(git tag | grep ^ckan-$CKANVERSION | sort --version-sort | tail -n 1)
+git checkout $CKAN_TAG
+echo "CKAN version: ${CKAN_TAG#ckan-}"
 
-if [ $CKANVERSION == '2.7' ]
-then
-    echo "Installing setuptools"
-    pip install setuptools==39.0.1
-fi
-
-python setup.py develop
 pip install -r requirements.txt
 pip install -r dev-requirements.txt
+pip install -e .
+
 cd -
 
 echo "Creating the PostgreSQL user and database..."
@@ -50,9 +34,5 @@ echo "Installing saml2 requirements..."
 pip install -r dev-requirements.txt
 echo "Installing ckanext-saml2auth..."
 pip install -e .
-
-echo "Moving test.ini into a subdir..."
-mkdir subdir
-mv test.ini subdir
 
 echo "setup-ckan.bash is done."
